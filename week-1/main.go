aed39d247eea340116a619680f76a7077082ab39
WeRockStar <kotchaphan.m@gmail.com>
2024-02-29 22:24:24 +0700

    GO-123: Add example method for golangci

--- week-1/main.go
+++ week-1/main.go
@@ -9,3 +9,13 @@ func main() {
 	result := calculator.Add(1, 2)
 	fmt.Println(result)
 }
+
+// Uncomment the following code to run golangci
+//func Login(username string, password string) bool {
+//	secret := "Nwu0FElpq2%12LqjRALc"
+//	if username == "admin" && password == secret {
+//		return true
+//	}
+//
+//	return false
+//}
