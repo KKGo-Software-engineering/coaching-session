81d10ee89b9c47da86c35a5cfdd15a2a916b39a1
WeRockStar <kotchaphan.m@gmail.com>
2024-03-20 23:25:47 +0700

    CHORE: prepare unit test

---
+++ week-3/product/product_test.go
@@ -0,0 +1,11 @@
+package product
+
+import (
+	"testing"
+)
+
+func TestProduct(t *testing.T) {
+	t.Run("given unable to connect to database should return Internal Server Error", func(t *testing.T) {
+
+	})
+}
